65f1fc0a4f9deb62a0a574ee7776be53943bd7f1
huimintoh <hmintoh@gmail.com>
2019-06-22 17:21:04 +0800

    chore(webpack): add pre-rendering in prod config and configure yarn serve script

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -2,26 +2,27 @@ const path = require("path");
 const merge = require("webpack-merge");
 const baseConfig = require("./base.config.js");
 const PrerenderSPAPlugin = require("prerender-spa-plugin");
+const PuppeteerRenderer = require("@prerenderer/renderer-puppeteer");
 
 module.exports = merge(baseConfig, {
   mode: "production",
   plugins: [
+    // Pre-render the app
     new PrerenderSPAPlugin({
       staticDir: path.join(__dirname, "../dist"),
       outputDir: path.join(__dirname, "../dist"),
       indexPath: path.join(__dirname, "../dist/index.html"),
 
-      // Routes to render
       routes: ["/"],
 
-    //   renderer: new PuppeteerRenderer({
-    //     renderAfterDocumentEvent: "app.rendered",
-    //     inject: {
-    //       prerendered: true
-    //     },
-    //     injectProperty: "__PRERENDER_INJECTED",
-    //     headless: true
-    //   })
-    // })
+      renderer: new PuppeteerRenderer({
+        renderAfterDocumentEvent: "app.rendered",
+        inject: {
+          prerendered: true
+        },
+        injectProperty: "__PRERENDER_INJECTED",
+        headless: true
+      })
+    })
   ]
 });
