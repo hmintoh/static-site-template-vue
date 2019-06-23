66f08b283f34a1a70f61437af22db9dbbabe79e2
huimintoh <hmintoh@gmail.com>
2019-06-23 16:32:31 +0800

    chore(webpack): add image loaders and optimisation in build config

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -3,6 +3,7 @@ const merge = require("webpack-merge");
 const baseConfig = require("./base.config.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const PrerenderSPAPlugin = require("prerender-spa-plugin");
 const PuppeteerRenderer = require("@prerenderer/renderer-puppeteer");
@@ -27,6 +28,36 @@ module.exports = merge(baseConfig, {
     ]
   },
   plugins: [
+    // Optimise images
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      jpegtran: {
+        // Lossless conversion to progressive.
+        progressive: true
+      },
+      optipng: {
+        optimizationLevel: 5
+      },
+      pngquant: {
+        // Instructs pngquant to use the least amount of colors required
+        // to meet or exceed the max quality.
+        // Min and max are numbers in range 0 (worst) to 1 (perfect), similar to JPEG.
+        quality: "50-80"
+      },
+      gifsicle: {
+        // Interlace gif for progressive rendering.
+        interlaced: true,
+        // Determines how much optimization is done
+        optimizationLevel: 3
+      },
+      svgo: {
+        options: {
+          cleanupAttrs: true,
+          removeDoctype: true
+        }
+      }
+    }),
+
     // Extract imported CSS into .css files
     new MiniCssExtractPlugin({
       filename: "[name].[hash].min.css",
